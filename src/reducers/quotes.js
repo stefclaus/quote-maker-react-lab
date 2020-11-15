615869eff8315d572db7aaf5da4063eec48eb5c8
github username <github email address>
2020-11-15 12:52:11 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
 export default (state = [], action) => {
-  return state;
+
+  let index;
+  let quote;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+
+    case 'REMOVE_QUOTE':
+    return state.filter(quote => quote.id !== action.quoteId);
+
+//    case 'UPVOTE_QUOTE'
+  //  return
+
+//    case 'DOWNVOTE_QUOTE'
+//    return
+
+      default:
+        return state;
+  }
 }
+
+
+
+//[
+//  {
+  //  id: '23423424242-42342423424242-fafdb',
+  //  content: 'One Awesome Quote',
+  //  author: 'Luke Ghenco'
+//  }
+// ]
